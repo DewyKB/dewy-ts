eaa9cda2d5c7d7c021271980aaa79943ae4b2232
Ryan Michael <kerinin@gmail.com>
2024-01-11 15:55:20 -0500

    Chat with docs example (in progress)

---
+++ examples/chat_with_docs/app/api/document/route.ts
@@ -0,0 +1,47 @@
+import KnowledgeBase from 'dewy-ts'
+import { NextRequest, NextResponse } from 'next/server';
+
+const kb = new KnowledgeBase({
+  apiKey: process.env.KB_API_KEY,
+})
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+    const formData = await req.formData();
+    const files = formData.getAll('files') as File[];
+    const fileToLoad = files[0];
+  
+    // Load a new file into the knowledge base
+    await kb.collection("docs").load( 
+        fileToLoad.name,                // An identifier for the document
+        fileToLoad,                     // The file to load
+        { 
+            await: 'stored',            // Wait for the document to be stored (but not necessarily indexed)
+            metadata: {user_id: "test"} // Assign some metadata for use with queries
+        } 
+    );
+}
+
+export async function PUT(req: Request) {
+    const { document_id } = await req.json()
+
+    const formData = await req.formData();
+    const files = formData.getAll('files') as File[];
+    const fileToLoad = files[0];
+  
+    // Update the contents of a file in the knowledge base
+    await kb.collection("docs").update( 
+        document_id,                    // The document to update
+        fileToLoad,                     // The new file to use with this ID
+        { await: 'indexed' }            // Wait for the new file to be stored and indexed
+    );
+}
+
+export async function DELETE(req: Request) {
+    const { document_id } = await req.json()
+
+    // Remove the document from the KB
+    await kb.collection("docs").delete(document_id);       
+}
---
+++ examples/chat_with_docs/app/api/route.ts
@@ -0,0 +1,67 @@
+import OpenAI from 'openai';
+import { Message, OpenAIStream, StreamingTextResponse } from 'ai'
+import KnowledgeBase from 'dewy-ts'
+import moment from 'moment';
+
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const kb = new KnowledgeBase({
+  apiKey: process.env.KB_API_KEY,
+})
+
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+  try {
+
+    const { messages, user_id } = await req.json()
+
+    // Get the last message
+    const lastMessage = messages[messages.length - 1]
+
+    // Query related information from the knowledge base
+    const docs = await kb.collection("docs").retrieve({
+      query: lastMessage.content, 
+      where: {owner: user_id, $created_at: {$gt: moment().subtract(1, 'days')}},
+      limit: 10,
+      order: 'cohere',
+    })
+
+    const prompt = [
+      {
+        role: 'system',
+        content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+      The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+      AI is a well-behaved and well-mannered individual.
+      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+      AI assistant is a big fan of Pinecone and Vercel.
+      START CONTEXT BLOCK
+      ${context}
+      END OF CONTEXT BLOCK
+      AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+      If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+      AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+      AI assistant will not invent anything that is not drawn directly from the context.
+      `,
+      },
+    ]
+
+    // Ask OpenAI for a streaming chat completion given the prompt
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages: [...prompt, ...messages.filter((message: Message) => message.role === 'user')]
+    })
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response)
+    // Respond with the stream
+    return new StreamingTextResponse(stream)
+  } catch (e) {
+    throw (e)
+  }
+}
